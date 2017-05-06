47dc8e5e5e10f56ebf39b2555f8ce9f47eb92b00
trueno20 <infblunaontiveros@gmail.com>
2017-05-06 02:38:32 -0500

    Add files via upload

---
+++ operacionesmatematicas.pl
@@ -0,0 +1,4 @@
+suma(Sumando1,Sumando2,Resultado):-Resultado is Sumando1+Sumando2.
+resta(Restado1,Restado2,Resultado):-Resultado is Restado1-Restado2.
+multiplicacion(Multiplica1,Multiplica2,Resultado):-Resultado is Multiplica1*Multiplica2.
+division(Dividiendo1,Dividiendo2,Resultado):-Resultado is Dividiendo1/Dividiendo2.
